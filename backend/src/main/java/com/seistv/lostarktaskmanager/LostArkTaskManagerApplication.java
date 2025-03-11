30b17bda9aa6e28502c9dbcca589d49eb4d2dea5
seistv <yue_03@yahoo.com>
2025-03-11 17:19:29 +0800

    Initialized backend and frontend

---
+++ backend/src/main/java/com/seistv/lostarktaskmanager/LostArkTaskManagerApplication.java
@@ -0,0 +1,13 @@
+package com.seistv.lostarktaskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostArkTaskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostArkTaskManagerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/seistv/lostarktaskmanager/LostArkTaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seistv.lostarktaskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostArkTaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
